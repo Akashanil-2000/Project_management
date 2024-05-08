3502cdc030ada053ab6faaf019da565246a22537
Akashanil-2000 <chazeakash@gmail.com>
2024-05-08 11:43:00 +0530

    Added register and login functions

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from backend import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,58 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from .models import Project, Todo
+from django.shortcuts import render, redirect
+
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.admin.views.decorators import staff_member_required
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def is_admin(user):
+    return user.is_superuser
+
+
+admin_required = user_passes_test(lambda user: user.is_superuser)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user)
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'todoApp/register.html', {'form': form})
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if user.is_superuser:  # If the user is an admin
+                return redirect('project_list')
+            else:  # If the user is a normal user
+                return redirect('user_todo_list')
+    else:
+        form = LoginForm()
+    return render(request, 'todoApp/login.html', {'form': form})
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
