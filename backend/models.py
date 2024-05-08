e0a43ac85e91acff896aec6dad982a3f14698353
Akashanil-2000 <chazeakash@gmail.com>
2024-05-08 10:52:10 +0530

    Added models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100,default=1)
+    created_date = models.DateTimeField()
+
+
+class Todo(models.Model):
+    name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,default=1)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    description = models.TextField(default='')
